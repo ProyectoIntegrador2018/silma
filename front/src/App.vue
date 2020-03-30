d92daa9b4fd1b1c9ba33f0e1238cc3ca44442dff
uriel96 <usu.96@hotmail.com>
2020-03-29 20:30:28 -0600

    Initial project setup for frontend and backend

---
+++ front/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app id="app">
+    <router-view />
+  </v-app>
+</template>
+<script>
+import axios from "axios";
+
+export default {
+  async mounted() {
+    const response = await axios.get("http://localhost:3000");
+    console.log(response.data);
+  }
+};
+</script>
+<style>
+</style>
---
+++ front/src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Hi</h1>
+    <v-btn depressed small color="primary">Vuetify is working</v-btn>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
