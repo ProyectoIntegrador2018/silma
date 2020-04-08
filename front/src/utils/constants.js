8af526f3bd01a41d61f769f79fe519c9395ca62b
Alfredo <alfredo.aj.17@gmail.com>
2020-04-07 23:14:11 -0500

    contsants & complete form

---
+++ front/src/utils/constants.js
@@ -0,0 +1,23 @@
+export const genres = [
+    "Sobrenatural (paranormal)",
+    "Romance",
+    "Aventura",
+    "Fantasía épica (de héroes)",
+    "Fantasía histórica",
+    "Realismo mágico",
+    "Chicas mágicas",
+    "Fantasía tecnológica (ciencia ficción)",
+    "Fantasía oscura",
+    "Steampunk",
+    "Terror",
+    "Fantasía infantil",
+    "Otros"
+];
+
+export const administrators = [
+    "Yolanda Chapa",
+    "Aurora Carranza",
+    "Anagaby Saban",
+    "Lorena Martinez",
+    "Nadie / Otra persona",
+];
--- front/src/views/reader/ReaderRegister.vue
+++ front/src/views/reader/ReaderRegister.vue
@@ -1,8 +1,12 @@
 <template>
   <v-container>
     <div class="display-3 font-weight-medium" align="center">Registro de Lector Beta</div>
+    <br>
     <v-form ref="form">
       <v-layout row wrap>
+        <v-flex xs6 offset-xs3>
+          <v-header>Datos personales</v-header>
+        </v-flex>
         <v-flex xs6 offset-xs3>
           <v-text-field
             label="Nombre Completo"
@@ -23,15 +27,50 @@
         </v-flex>
         <v-flex xs6 offset-xs3>
           <v-text-field
+            label="Perfil de Facebook"
+            outline
+          ></v-text-field>
+        </v-flex>
+        <v-flex xs6 offset-xs3>
+          <v-text-field
             :type="showPassword ? 'text' : 'password'"
             name="input-10-1"
-            label="Password"
+            label="Contraseña"
             counter
             outline
             @click:append="showPassword = !showPassword"
           ></v-text-field>
         </v-flex>
-        
+        <v-flex xs6 offset-xs3>
+          <v-header>Datos para Silma</v-header>
+        </v-flex>
+        <br>
+        <v-flex xs6 offset-xs3>
+          <v-subheader>¿Cuánto te tardas en leer 200 páginas?</v-subheader>
+          <v-radio-group>
+            <v-radio label="3 días o menos" value="fast"></v-radio>
+            <v-radio label="5 días" value="average"></v-radio>
+            <v-radio label="7 días o más" value="slow"></v-radio>
+          </v-radio-group>
+        </v-flex>
+        <v-flex xs6 offset-xs3>
+          <v-subheader>¿Quién te recomendó?</v-subheader>
+          <v-col class="d-flex" cols="12" sm="6">
+            <v-select
+              :items="administrators"
+              dense
+            ></v-select>
+          </v-col>
+        </v-flex>
+        <br>
+        <v-flex xs6 offset-xs3>
+          <v-header>Preferencias de lectura</v-header>
+        </v-flex>
+        <v-flex xs6 offset-xs3 v-for="genres in genres" :key="genres">
+          <v-checkbox
+            :label="genres"
+          ></v-checkbox>
+        </v-flex>
       </v-layout>
     </v-form>
     <div align="center" class="mt-3 mb-5">
@@ -42,6 +81,16 @@
 </template>
 
 
-<script></script>
+<script>
+import {genres, administrators} from '@/utils/constants.js';
+export default{
+  data(){
+    return {
+      genres,
+      administrators
+    }
+  }
+}
+</script>
 
 <style scoped></style>
