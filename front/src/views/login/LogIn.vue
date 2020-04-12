dc4ba190db6196ba3745b02f81cadc669f918807
Luisa <fernandapinedaochoa@gmail.com>
2020-04-11 21:54:44 -0500

    Log in information authenticated

--- front/src/views/login/LogIn.vue
+++ front/src/views/login/LogIn.vue
@@ -6,10 +6,11 @@
     <br>
     <br>
     <div align="center" >
-    <v-form ref="form" method="POST" action="/login">
+    <v-form ref="form">
         <v-text-field
               outlined
               :rules="[emailRule]"
+              v-model="user.email"
               label="E-mail"
               style="width: 50%"
             ></v-text-field>
@@ -21,40 +22,99 @@
               @click:append="showPassword = !showPassword"
               :type="showPassword ? 'text' : 'password'"
               name="input-10-1"
+              v-model="user.password"
               style="width: 50%"
             ></v-text-field>
             <br>
-         <v-btn class="btn btn-indigo" type="submit">Login</v-btn>
+         
+
+    <v-layout row wrap>
+      <v-dialog v-model="dialogSuccess" persistent max-width="290">
+        <v-card>
+          <v-card-title class="headline">¡Bienvenido a tu sesión!</v-card-title>
+          <v-card-text>Serás reenviado a tu dashboard</v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="green darken-1" text @click="dialogSuccess = false">Entrar</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+
+      <v-dialog v-model="dialogError" persistent max-width="290">
+        <v-card>
+          <v-card-title class="headline">Error al iniciar sesión</v-card-title>
+          <v-card-text>Por favor, inténtelo más tarde</v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="red darken-1" text @click="dialogError = false">Entendido</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+
+    </v-layout>
+
+    <v-dialog v-model="dialogIncorrectInfo" persistent max-width="290">
+        <v-card>
+          <v-card-title class="headline">Datos Incorrectos</v-card-title>
+          <v-card-text>La información ingresada no es correcta</v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="red darken-1" text @click="dialogIncorrectInfo = false">Entendido</v-btn>
+            
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+
+        <v-btn @click="login" class="btn btn-indigo">Login</v-btn>
          <br>
          <br>
-         <a > ¿No tienes cuenta? </a>
+         <p > ¿No tienes cuenta? </p>
          <a href="/Registro_Lector" class="button">Registrarme como Lector</a>
+         <br>
+         <a href="/Registro_Escritor" class="button">Registrarme como Escritor</a>
     </v-form>
     </div>
   </v-container>
 </template>
 
 <script>
+import axios from 'axios';
 import {emailRule} from '@/utils/rules';
 export default {
 
     data(){
         return {
+            user: {
+                email:'',
+                password:''
+            },
             showPassword: false,
-            emailRule
+            emailRule,
+            dialogSuccess: false,
+            dialogError: false,
+            dialogIncorrectInfo: false
         }
     },
     methods: {
-        /*login(){
+        async login() {
             try {
-                this.axios.post('http://localhost:8000/api/user/authentication', {
-                    name: this.name,description: this.description                 
-                    })
-            } catch(error) {
-                alert("Problema del sitio")
-                console.log(error)
+                const authUser = {
+                    email: this.user.email,
+                    password: this.user.password
+                }
+                const responseAuth = await axios.post("http://localhost:3000/api/user/authentication", authUser)
+                console.log(responseAuth)
+                if (responseAuth.data.token != null){
+                    //this.dialogSuccess = true
+                    document.cookie = "token=" + responseAuth.data.token;
+                    location.href = '/dashboard';
+                } else {
+                    this.dialogIncorrectInfo = true
+                }
+            } catch (error) {
+                this.dialogError = true
             }
-        }*/
+        }
     }
 }
 </script>
