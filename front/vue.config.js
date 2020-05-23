07c9c6836d6daa37e71a1f5ffc54cd48ddd209d2
uriel96 <usu.96@hotmail.com>
2020-05-23 14:50:56 -0500

    Added heroku config

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
 
+/*
 if (process.env.NODE_ENV !== 'production') {
   process.env.VUE_APP_API_HOST = 'http://localhost:3000/api';
 }
+*/
 
 module.exports = {
   outputDir: path.resolve(__dirname, '../back/dist/public')
