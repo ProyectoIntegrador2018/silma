6f80ffa241a8e04ee51f85dc5e35db26f1edfa07
Ivanovic27 <ivanrmzmtz@gmail.com>
2020-05-24 13:11:12 -0500

    Initialized Admin

---
+++ front/src/components/dashboardAdmin.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <h1>Initialized Admin</h1>
+    </div>
+
+</template>
+
+
+<script>
+
+export default {
+  components: {
+    
+  },
+  data() {
+    
+  },
+  created() {
+  },
+  asyncComputed: {
+     
+  },
+  methods: {
+   
+  },
+};
+</script>
