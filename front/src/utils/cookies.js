b3593f0625823a1245d06943f4f51a8418c09b2c
Uriel96 <usu.96@hotmail.com>
2020-05-25 00:30:42 -0500

    Changed toolbar with ability to change role (#37)

--- front/src/utils/cookies.js
+++ front/src/utils/cookies.js
@@ -6,8 +6,8 @@ export const setAuthCookies = (user) => {
     if (!Vue.$cookies.isKey('user_type')) {
       const role = user.roles.includes('admin')
         ? 'admin' : user.roles.includes('writer')
-        ? 'writer' : 'reader';
-        Vue.$cookies.set('user_type', role);
+          ? 'writer' : 'reader';
+      Vue.$cookies.set('user_type', role);
       Vue.$cookies.set('user_id', user._id);
       Vue.$cookies.set('user_name', user.name);
     }
